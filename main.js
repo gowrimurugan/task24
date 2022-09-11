6e0153b19a38610f18504656edc0b95e05ac8314
gowrimurugan <107912689+gowrimurugan@users.noreply.github.com>
2022-09-11 17:46:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let num = prompt("Enter The Number") 
+let range = prompt  ("Enter The Range")
+
+for  (let i = 1; i<=range; i++){
+   let table = num * i;
+    console.log (i + "x" + num + "=" + table);
+}
